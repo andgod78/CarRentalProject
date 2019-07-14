f536197a91e0caf5e60abfdbf36edd283cc0869a
Marek <jastrzebski.m97@gmail.com>
2019-07-14 13:13:52 +0200

    added DeleteCar implementation

--- src/main/java/pl/com/carrental/model/CarRental.java
+++ src/main/java/pl/com/carrental/model/CarRental.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class CarRental {
     public static List<Car> createCars(){
         List<Car> carList = new ArrayList<Car>();
-        carList.add(new Car(1,"Fiat","126p",1982,true,999999,1));
-        carList.add(new Car(2,"Skoda","Fabia",1999,false,129873,20));
-        carList.add(new Car(3,"BMW","E39",2000,false,781982,30));
-        carList.add(new Car(1,"Opel","Calibra",1992,false,876122,45));
-        carList.add(new Car(1,"Toyota","Yaris",2014,true,12000,60));
-        carList.add(new Car(1,"WV","Passat",2018,false,200000,10));
+        carList.add(new Car(1,"Fiat","126p",1982,999999,1));
+        carList.add(new Car(2,"Skoda","Fabia",1999,129873,20));
+        carList.add(new Car(3,"BMW","E39",2000,781982,30));
+        carList.add(new Car(1,"Opel","Calibra",1992,876122,45));
+        carList.add(new Car(1,"Toyota","Yaris",2014,12000,60));
+        carList.add(new Car(1,"WV","Passat",2018,200000,10));
         return carList;
     }
 }
